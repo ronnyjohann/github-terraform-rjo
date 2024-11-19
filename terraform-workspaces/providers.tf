4abad61b1f80f3e033ebd4c8049a5200fcf59406
ronnyjohann <ronnyjohann@gmail.com>
2024-11-19 19:27:00 +0100

    Legg til GitHub Actions arbeidsflyt

---
+++ terraform-workspaces/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
+    }
+  }
+
+  # Backend-konfigurasjon for lagring av tilstand
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-rjo"
+    storage_account_name = "sabackendrjo1bwdbknuve"
+    container_name       = "sc-backend-container-rjo"
+    key                  = "TFWorkPlace.terraform.tfstate"
+  }
+}
+
+# Provider-konfigurasjon for azurerm med features
+provider "azurerm" {
+  features {}
+}
